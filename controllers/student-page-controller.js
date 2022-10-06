68dc12e59c07f7a7046ab471059d7657337e6327
diaabadr <diaabadr82@gmail.com>
2022-10-06 23:51:37 +0200

    student page finished with its check Auth

---
+++ controllers/student-page-controller.js
@@ -0,0 +1,107 @@
+var express = require("express");
+var router = express.Router();
+const User = require("../Models/user");
+const Courses = require("../Models/courses");
+const Quizes = require("../Models/quiz");
+const Skills = require("../Models/skills");
+const { calc_percentage } = require("./admin-panel-controller");
+const jwt = require("jsonwebtoken");
+
+const main_page = async function (req, res, next) {
+  const token = req.cookies.jwt;
+  const decoded = await jwt.verify(token, "ODC");
+  const userId = decoded.id;
+  console.log(userId);
+  const user = await User.findById(userId, {
+    name: 1,
+    email: 1,
+    phone: 1,
+    skills: 1,
+    courses: 1,
+    _id: 0,
+  });
+
+  const unenrolled_courses = await Courses.find(
+    { name: { $nin: user.courses } },
+    { name: 1, _id: 1 }
+  );
+  res.status(201).json({ user, unenrolled_courses });
+};
+
+const enroll_course = async (req, res) => {
+  const { id } = req.body;
+  const course = await Courses.findById(id);
+  res.status(201).json({
+    course_id: course._id,
+    quiz_name: course.Prerequisite_quiz,
+    type: "enrollcourse",
+  });
+};
+
+const take_quiz = async (req, res) => {
+  const { quiz_name } = req.body;
+  const quiz = await Quizes.findOne(
+    { name: quiz_name },
+    { questions: { answer: 0 } }
+  );
+  res.status(201).json(quiz);
+};
+
+const submit_quiz = async (req, res) => {
+  let { course_id, skill, quiz_id } = req.body;
+  let { t: type } = req.query;
+  if (skill) {
+    const skill_quiz = await Skills.findOne({ name: skill }, { quiz: 1 });
+    console.log(skill_quiz);
+    const quiz = await Quizes.findOne({ name: skill_quiz.quiz });
+    quiz_id = quiz._id;
+  }
+  const { quiz_answers } = req.body;
+  let student_answers_map = new Map();
+  for (const q of quiz_answers) {
+    student_answers_map.set(q.id, q.ans);
+  }
+  const { questions: right_answers, passing_percentage } =
+    await Quizes.findById(quiz_id, {
+      questions: { question: 0 },
+      name: 0,
+      _id: 0,
+    });
+  let grade = 0;
+  for (const ans of right_answers) {
+    const student_answer = student_answers_map.get(ans._id.toString());
+    if (student_answer === ans.answer) grade++;
+  }
+  const quiz_result = calc_percentage(grade, right_answers.length);
+  if (quiz_result < passing_percentage) {
+    res.status(201).json({ message: "Failed to pass the exam" });
+    return;
+  }
+
+  const token = req.cookies.jwt;
+  const decoded = await jwt.verify(token, "ODC");
+  const userId = decoded.id;
+  const user = await User.findById(userId);
+
+  if (type === "enrollcourse") {
+    const course = await Courses.findById(course_id, { name: 1 });
+    const updated_user = await User.updateOne(
+      { _id: user._id.toString() },
+      { $push: { courses: course.name } }
+    );
+    res.redirect("/student/" + user.id);
+  } else {
+    const skill_inserted = await Skills.findOne({ name: skill }, { name: 1 });
+    const updated_user = await User.updateOne(
+      { _id: user._id.toString() },
+      { $push: { skills: skill_inserted.name } }
+    );
+    res.redirect("/student/" + user.id);
+  }
+};
+
+const add_skill= async (req, res) => {
+  const skills = await Skills.find({}, { name: 1 });
+  res.status(201).json(skills);
+}
+module.exports = { main_page, enroll_course, take_quiz,submit_quiz,add_skill };
--- routes/admin.js
+++ routes/admin.js
@@ -1,73 +1,63 @@
 var express = require("express");
 var router = express.Router();
-const jwt = require("jsonwebtoken");
-const Courses = require("../Models/courses");
-const Skills = require("../Models/skills");
-const Quizes = require("../Models/quiz");
-const Suppliers = require("../Models/suppliers");
-const User = require("../Models/user");
 const { requireAuth } = require("../middleware/auth");
-const admin_panel_controller = require("../controllers/admin-panel-controller");
-const students_data_controller = require("../controllers/students-data-controller");
-const courses_data_controller = require("../controllers/courses-data-controller");
-const recommendation_system_controller = require("../controllers/recommendation-system-controller");
+const {
+  main_page,
+  new_courses_form,
+  new_skill_form,
+  add_course,
+  add_skill,
+  add_quiz,
+  add_supplier,
+} = require("../controllers/admin-panel-controller");
+const {
+  students_data,
+  all_student_info,
+} = require("../controllers/students-data-controller");
+const { courses_data } = require("../controllers/courses-data-controller");
+const {
+  most_matching_students,
+  recommend_student,
+} = require("../controllers/recommendation-system-controller");
+
+
 
 /* GET users listing. */
 
 // admin panel
-router.get("/:id", requireAuth, admin_panel_controller.main_page);
+router.get("/:id", requireAuth, main_page);
 
 // get new course form data
-router.post(
-  "/newcourseform",
-  requireAuth,
-  admin_panel_controller.new_courses_form
-);
+router.post("/newcourseform", requireAuth, new_courses_form);
 
 // feature add course
-router.post("/addcourse", requireAuth, admin_panel_controller.add_course);
+router.post("/addcourse", requireAuth, add_course);
 
 // get new skill form data (quizes available and )
-router.post(
-  "/newskillform",
-  requireAuth,
-  admin_panel_controller.new_skill_form
-);
+router.post("/newskillform", requireAuth, new_skill_form);
 
 // feature add new skill
-router.post("/addskill", requireAuth, admin_panel_controller.add_skill);
+router.post("/addskill", requireAuth, add_skill);
 
 // feature add new quiz
-router.post("/addquiz", requireAuth, admin_panel_controller.add_quiz);
+router.post("/addquiz", requireAuth, add_quiz);
 
 // get students info
-router.post("/students", requireAuth, students_data_controller.students_data);
+router.post("/students", requireAuth, students_data);
 
 // get all info of the student
-router.post(
-  "/studentinfo",
-  requireAuth,
-  students_data_controller.all_student_info
-);
+router.post("/studentinfo", requireAuth, all_student_info);
 
 // get courses info
-router.post("/courses", requireAuth, courses_data_controller.courses_data);
+router.post("/courses", requireAuth, courses_data);
 
 // adding supplier
-router.post("/addsupplier", requireAuth, admin_panel_controller.add_supplier);
+router.post("/addsupplier", requireAuth, add_supplier);
 
 // recommend students based on some skills
-router.post(
-  "/recommend",
-  requireAuth,
-  recommendation_system_controller.most_matching_students
-);
+router.post("/recommend", requireAuth, most_matching_students);
 
 // send mail to student about the job
-router.post(
-  "/recommendstudent",
-  requireAuth,
-  recommendation_system_controller.recommend_student
-);
+router.post("/recommendstudent", requireAuth, recommend_student);
 
 module.exports = router;
